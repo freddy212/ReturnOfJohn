9e80b95977aa154e1a97ebe67da70330313179ab
freddy212 <201406525@post.au.dk>
2023-06-29 14:18:38 +0200

    Ice Clone explotion effect

--- core/src/com/mygdx/game/ItemAbilities/IceCloneAbilityUpgraded.kt
+++ core/src/com/mygdx/game/ItemAbilities/IceCloneAbilityUpgraded.kt
@@ -1,9 +1,17 @@
 package com.mygdx.game.ItemAbilities
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.ParticleEffect
 import com.badlogic.gdx.math.Vector2
 import com.mygdx.game.AbstractClasses.AbilityId
+import com.mygdx.game.AbstractClasses.GameObject
+import com.mygdx.game.Animation.ParticleAnimation
+import com.mygdx.game.Enums.Layer
+import com.mygdx.game.GameObjects.MoveableEntities.IceClone
 import com.mygdx.game.GameObjects.MoveableEntities.Projectiles.Icicle
+import com.mygdx.game.Managers.AnimationManager
 import com.mygdx.game.Managers.LocationManager
+import com.mygdx.game.ObjectProperties.ROJParticleObject
 import com.mygdx.game.player
 import com.mygdx.game.plus
 
@@ -19,6 +27,7 @@ class IceCloneAbilityUpgraded: IceCloneAbility() {
             icicle.addToLocation(iceClone.defaultLocation!!)
             icicle2.addToLocation(iceClone.defaultLocation!!)
             iceClone.removeFromLocation()
+            AnimationManager.animationManager.add(IceCloneExplotionAnimation(iceClone))
         }else{
             super.activeAction()
         }
@@ -27,5 +36,21 @@ class IceCloneAbilityUpgraded: IceCloneAbility() {
         val iceClone = player.itemAbilities.List.firstOrNull { it is IceCloneAbility }
         player.removeAbility(iceClone)
         super.handleAbilityGained()
+    }
+}
+
+class IceCloneExplotionAnimation(iceClone: IceClone) : ParticleAnimation() {
+    override val duration = 60
+    override val animationAction = {}
+    override var actionFrame = 60
+    override val animationEffect = IceCloneExplotionEffect(iceClone)
+    override val layer = Layer.PERSON
+}
+
+class IceCloneExplotionEffect(objectAttached: GameObject,  posModifier: Vector2 = Vector2(objectAttached.sprite.width/2, objectAttached.sprite.height/2)): ROJParticleObject(
+    ParticleEffect(), objectAttached, posModifier) {
+    override val layer = Layer.PERSON
+    init {
+        particleEffect.load(Gdx.files.internal("ParticleEmitters/IceCloneEffect.p"), Gdx.files.internal(""))
     }
 }
