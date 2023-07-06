016d3b6430df71b1d94dd4196f81466632677758
freddy212 <201406525@post.au.dk>
2023-07-06 13:05:36 +0200

    made it obvious when ability upgraded

--- core/src/com/mygdx/game/Signal/SignalListeners/AddAbilityItem.kt
+++ core/src/com/mygdx/game/Signal/SignalListeners/AddAbilityItem.kt
@@ -1,6 +1,8 @@
 package com.mygdx.game.Signal.SignalListeners
 
 import com.badlogic.gdx.math.Vector2
+import com.mygdx.game.AbstractClasses.CharacterAbility
+import com.mygdx.game.AbstractClasses.GameObject
 import com.mygdx.game.AbstractClasses.ToggleAbility
 import com.mygdx.game.AbstractClasses.getAbility
 import com.mygdx.game.GameObjects.ItemObjects.AbilityItemObject
@@ -8,9 +10,8 @@ import com.mygdx.game.GameObjects.MoveableEntities.Projectiles.Fireball
 import com.mygdx.game.GameObjects.MoveableEntities.Projectiles.Icicle
 import com.mygdx.game.GameObjects.MoveableEntities.Projectiles.SmallBoulder
 import com.mygdx.game.GameObjects.MoveableEntities.Projectiles.WaterBall
-import com.mygdx.game.ItemAbilities.FireballAbility
-import com.mygdx.game.ItemAbilities.IcicleAbility
-import com.mygdx.game.ItemAbilities.WaterBallAbility
+import com.mygdx.game.GameObjects.Other.Sign
+import com.mygdx.game.ItemAbilities.*
 import com.mygdx.game.Managers.LocationManager
 import com.mygdx.game.Signal.Signal
 import com.mygdx.game.Signal.SignaledEventListener
@@ -28,5 +29,23 @@ class AddAbilityItem: SignaledEventListener {
         val abilityItemObject = AbilityItemObject(Vector2(addAbilityItemSignal.x, addAbilityItemSignal.y), Vector2(50f,50f), location, characterAbility)
 
         abilityItemObject.addToLocation(location)
+
+        if(characterAbility is ShieldAbilityUpgraded || characterAbility is IceCloneAbilityUpgraded || characterAbility is DashAbilityUpgraded){
+            val sign = getSign(characterAbility, abilityItemObject)
+            sign.addToLocation(location)
+        }
+    }
+
+    fun getSign(characterAbility: CharacterAbility, abilityObject: GameObject): Sign{
+
+        val signText = when(characterAbility){
+            is ShieldAbilityUpgraded -> Pair("Shield Upgraded", "Shield is now fire and ice resistant")
+            is IceCloneAbilityUpgraded -> Pair("Ice Clone Upgraded", "Press twice to create ice explotion")
+            is DashAbilityUpgraded -> Pair("Dash Upgraded", "Invulnerable while dashing")
+            else ->  Pair("Random", "Random")
+        }
+
+        return Sign(abilityObject.initPosition, Vector2(80f,80f), abilityObject.defaultLocation, signText.first, signText.second)
+
     }
 }
--- core/src/com/mygdx/game/UI/ToolTipPanel/ToolTip.kt
+++ core/src/com/mygdx/game/UI/ToolTipPanel/ToolTip.kt
@@ -1,13 +1,21 @@
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.mygdx.game.AbstractClasses.CharacterAbility
 import com.mygdx.game.Interfaces.Timer
+import com.mygdx.game.ItemAbilities.DashAbilityUpgraded
+import com.mygdx.game.ItemAbilities.IceCloneAbilityUpgraded
+import com.mygdx.game.ItemAbilities.ShieldAbilityUpgraded
+import com.mygdx.game.Managers.UIRendererManager
 import com.mygdx.game.Timer.DefaultTimer
 import com.mygdx.game.font
 
 class ToolTip (val key: Char, val characterAbility: CharacterAbility) {
     var show = true
+    val shapeRenderer = UIRendererManager.uiShapeRenderer
     fun render(position: Vector2,UIbatch: PolygonSpriteBatch){
         if(characterAbility.showToolTip){
             val sprite = Sprite(characterAbility.toolTipTexture)
@@ -17,6 +25,17 @@ class ToolTip (val key: Char, val characterAbility: CharacterAbility) {
             sprite.setAlpha(alphaValue)
             sprite.draw(UIbatch)
             font.draw(UIbatch,key.toString(),sprite.x + sprite.width / 3, sprite.y + sprite.height + 20f)
+
+            if(characterAbility is DashAbilityUpgraded || characterAbility is ShieldAbilityUpgraded || characterAbility is IceCloneAbilityUpgraded){
+                UIbatch.end()
+                Gdx.gl.glLineWidth(3f);
+                shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
+                shapeRenderer.color = Color.GREEN
+                shapeRenderer.circle(sprite.x + sprite.width / 2, sprite.y + sprite.height / 2, 30f)
+                shapeRenderer.end()
+                Gdx.gl.glLineWidth(1f);
+                UIbatch.begin()
+            }
         }
     }
 }
